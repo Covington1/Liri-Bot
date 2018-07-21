8c392fe53a2d6148f44ac47ac07d68dc11dd6313
Tee <teecovington@gmail.com>
2018-07-21 15:03:19 -0400

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+# liri-bot-app
+liri-bot-app
+
+With this app, it can take in parameters on the command line and return data.
---
+++ keys.js
@@ -0,0 +1,25 @@
+/*console.log('this is loaded');*/
+//API Keys here. There isn' a ".env file"
+
+var twitterKeys = {
+  consumer_key: '4CLJiskLMV00Y5PyqMl26nzdD',
+  consumer_secret: 'Bf1ddBtK0HL31O1TORgWtL1SGg50Kv45ICqat1p7UBW16nEpfN',
+  access_token_key: '764567174564372480-WUoWSQrxaZQLEXh8mIsGEKPB47VJWWN',
+  access_token_secret: 'ACYOWDiEeBRlWN0xSM4pMgDAda33SNkjaQbd0GUHeVyaX',
+};
+
+var spotifKey = {
+  id: '4011a7ff05e442af8f826fde23f7b01f',
+  secret: 'aafa088b788c4e67b6b269b5ecb627cd'
+};  
+
+var omdbKey = {
+    API_key: '40e9cece'
+};
+
+var apiKeys = {
+    twitter: twitterKeys,
+    spotify: spotifKey,
+    omdb: omdbKey
+}
+module.exports = apiKeys;
